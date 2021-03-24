0c653438adcdd0405a4944351ba956fd4ead15cf
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2021-03-23 21:37:33 -0300

    feat: introducing base project

---
+++ src/test/java/br/com/spring/hateoas/HateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
